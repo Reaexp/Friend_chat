1e2980c6dfcac8a070ca4016481017d21ade446b
Reaexp <reaexp.it@gmail.com>
2023-01-19 15:07:10 +0300

    test1

---
+++ main.py
@@ -0,0 +1,23 @@
+import openai
+import telebot
+
+bot = telebot.TeleBot('test')
+openai.api_key = 'test'
+
+@bot.message_handler(func=lambda _: True)
+def handler_message(message):
+
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt= message.text,
+    temperature=0.5,
+    max_tokens=1000,
+    top_p=1.0,
+    frequency_penalty=0.5,
+    presence_penalty=0.0,
+
+  )
+
+  bot.send_message(chat_id=message.from_user.id, text=response['choices'][0]['text'])
+
+bot.polling()
